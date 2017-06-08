dc3f9b9de23c0d24e199e655d1a99628207ff310
AlfredoRatesRaig <alfredo.rates@raig.uchile.cl>
2017-06-08 11:40:43 -0400

    Cambios prueba 05/06/2017

--- Pruebas.X/prueba.c
+++ Pruebas.X/prueba.c
@@ -17,45 +17,35 @@ int main()
 { 
     TRISB = 0xFF;                   //Declaración de puertos
     TRISD = 0x00;
-    TRISC = 0x00;
-    TRISCbits.RC6 = 1;
-    
-    PORTCbits.RC0 = 1;                        //Condiciones Iniciales
+    TRISC = 0b01000000;
+    TMR1CS = 0;
+    TMR3CS = 0;
+    //PORTCbits.RC0 = 1;                        //Condiciones Iniciales
     PORTCbits.RC1 = 1;
     PORTCbits.RC2 = 1;
     PORTCbits.RC7 = 0;
-    
+    Input1 = 0xFF;
+    Input2 = 0x00;
     while(1) 
     {
-        PORTCbits.RC0 = 0;                    //Mux en 00
+        //PORTCbits.RC0 = 0;                    //Mux en 00
         PORTCbits.RC1 = 0;
         
         PORTCbits.RC2 = 1;                    //Subo RD
-        __delay_us(25); 
-        PORTCbits.RC2 = 0;                    //Bajo RD
-        while(PORTCbits.RC6 == 1){}           //Espero Flag
-        
+        __delay_us(25);
+        PORTCbits.RC2 = 0;                    //Subo RD
         Input1 = PORTB;             //Lectura y escritura
         PORTD = Input1;
-        
         __delay_ms(4000); 
-        
-        PORTCbits.RC0 = 1;                    //Mux en 11
+        //PORTCbits.RC0 = 1;                    //Mux en 11
         PORTCbits.RC1 = 1;
         
-        PORTCbits.RC2 = 1;                    //Subo RD
-        __delay_us(25); 
-        PORTCbits.RC2 = 0;                    //Bajo RD
-        while(PORTCbits.RC6 == 1)
-        {
-        
-        }           //Espero Flag
-        
+        PORTCbits.RC2 = 1;                    //Bajo RD
+        __delay_us(25);
+        PORTCbits.RC2 = 0;   
         Input2 = PORTB;             //Lectura y escritura
-        PORTD = Input2;
-        
+        PORTD = Input2;//Bajo RD
         __delay_ms(4000); 
-
     }
 return 0;
 }
